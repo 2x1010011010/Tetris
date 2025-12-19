812c9d3855530b0976d89faf24a3f5a255259460
Sergei Anisimov <s.anisimov.it@gmail.com>
2025-12-19 19:02:49 +0100

    Update GameStats.cpp

--- src/GameStats.cpp
+++ src/GameStats.cpp
@@ -9,3 +9,33 @@ void GameStats::checkLevelUp(unsigned int oldLines)
     }
 }
 
+void GameStats::updateScore(unsigned int lines)
+{
+    ScoreMultiplayer multiplayer;
+
+    switch (lines)
+    {
+        case 1:
+            multiplayer = ScoreMultiplayer::SINGLE_LINE;
+            break;
+        case 2:
+            multiplayer = ScoreMultiplayer::DOUBLE_LINE;
+            break;
+        case 3:
+            multiplayer = ScoreMultiplayer::TRIPLE_LINE;
+            break;
+        case 4:
+            multiplayer = ScoreMultiplayer::TETRIS;
+            break;
+    }
+
+    score_ += static_cast<int>(multiplayer) * (level_ + 1);
+}
+
+void GameStats::processLinesCleared(unsigned int linesClearedCount)
+{
+    updateScore(linesClearedCount);
+    const unsigned int oldLines = linesCleared_;
+    linesCleared_ += linesClearedCount;
+    checkLevelUp(oldLines);
+}
