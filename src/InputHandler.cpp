443d742e5d7a7b580952a055351f6851317e73a2
Sergei Anisimov <s.anisimov.it@gmail.com>
2025-12-20 14:45:47 +0100

    Update InputHandler.cpp

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -1,5 +1,53 @@
-//
-// Created by user on 12/17/2025.
-//
+#include "headers/InputHandler.h"
+#include "headers/GameController.h"
 
-#include "InputHandler.h"
+InputHandler::InputHandler(GameController& controller, sf::RenderWindow& window) :
+    controller_(controller),
+    window_(window)
+{
+    setupKeyBindings();
+}
+
+void InputHandler::handleKeyPress(sf::Keyboard::Key key)
+{
+    if (const auto pressedKey = keyBindings_.find(key); pressedKey != keyBindings_.end())
+        pressedKey->second();
+}
+
+void InputHandler::processEvent(const std::optional<sf::Event>& event)
+{
+    if (event->is < sf::Event::KeyPressed>())
+    {
+        const auto& keyEvent = event->getIf<sf::Event::KeyPressed>();
+        handleKeyPress(keyEvent->code);
+    }
+}
+
+void InputHandler::setupKeyBindings()
+{
+    keyBindings_[sf::Keyboard::Key::A] = [this](){controller_.handleLeftMove();};
+    keyBindings_[sf::Keyboard::Key::Left] = [this](){controller_.handleLeftMove();};
+
+    keyBindings_[sf::Keyboard::Key::D] = [this](){controller_.handleRightMove();};
+    keyBindings_[sf::Keyboard::Key::Right] = [this](){controller_.handleRightMove();};
+
+    keyBindings_[sf::Keyboard::Key::W] = [this](){controller_.handleRotate();};
+    keyBindings_[sf::Keyboard::Key::Up] = [this](){controller_.handleRotate();};
+
+    keyBindings_[sf::Keyboard::Key::S] = [this](){controller_.handleSoftDrop();};
+    keyBindings_[sf::Keyboard::Key::Down] = [this](){controller_.handleSoftDrop();};
+
+    keyBindings_[sf::Keyboard::Key::Space] = [this](){controller_.handleHardDrop();};
+
+    keyBindings_[sf::Keyboard::Key::P] = [this](){controller_.togglePause();};
+
+    keyBindings_[sf::Keyboard::Key::Escape] = [this](){window_.close();};
+
+    keyBindings_[sf::Keyboard::Key::Enter] = [this]()
+    {
+        if (controller_.getGameState() == GameState::Start)
+            controller_.startGame();
+        else if (controller_.getGameState() == GameState::GameOver)
+            controller_.restartGame();
+    };
+}
