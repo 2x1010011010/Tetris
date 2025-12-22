56d3ffad856ce6689bbbbb4f84d3f41df4f88c35
Sergei Anisimov <s.anisimov.it@gmail.com>
2025-12-22 17:24:26 +0100

    Update Tetromino.cpp

--- src/Tetromino.cpp
+++ src/Tetromino.cpp
@@ -9,4 +9,29 @@ Tetromino::Tetromino(const TetrominoType type, const sf::Vector2i startPos) : sh
 void Tetromino::updatePositions()
 {
     const auto coords = shape.getCoordinates();
+    positions.clear();
+    positions.reserve(4);
+
+    for (const auto& coord : coords)
+    {
+        const sf::Vector2i rotatedCoord = rotatePoint(coord.x, coord.y, rotationState);
+        positions.emplace_back(rotatedCoord.x + offset.x, rotatedCoord.y + offset.y);
+    }
 }
+
+sf::Vector2i Tetromino::rotatePoint(int x, int y, int rotation) const
+{
+    if (shape.getType() == TetrominoType::O)
+        return {x, y};
+
+    switch (rotation)
+    {
+    case 0: return {x, y};          //0
+    case 1: return {-y, x};     //90
+    case 2: return {-x, -y};        //180
+    case 3: return {y, -x};     //270
+    default: return {x, y};
+    }
+}
+
+
